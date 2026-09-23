78333be87bf2b02140b629565417230de56b2bc8
DrollRobot <glare.zen621@addymail.com>
2026-09-23 11:53:26 -0400

    feat(alias): move 'irt' alias to Connect-IRTenant

--- M365IncidentResponseTools.psm1
+++ M365IncidentResponseTools.psm1
@@ -28285,7 +28285,7 @@ function Import-IRT {
         entirely by PowerShell's automatic module loading when any exported function
         from the module is invoked.
     #>
-    [Alias('ImportIRT', 'LoadIRT', 'IRT')]
+    [Alias('ImportIRT', 'LoadIRT')]
     [CmdletBinding()]
     [OutputType([void])]
     param()
